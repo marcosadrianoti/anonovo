2cdf92f1e37b5cf8df358879789622ab3a66df01
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-04-27 17:20:09 -0300

    regression working

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const secondsContainer = document.querySelector('#seconds');
+const minutesContainer = document.querySelector('#minutes');
+const hoursContainer = document.querySelector('#hours');
+const daysContainer = document.querySelector('#days');
+
+
 const nextYear = new Date().getFullYear() + 1;
 const newYearTime = new Date(`january 01 ${nextYear} 00:00:00`);
 
@@ -9,8 +15,13 @@ const updateCountdown = () => {
     const minutes = Math.floor(difference / 1000 / 60) % 60;
     const seconds = Math.floor(difference / 1000) % 60;
 
-
-    console.log(days, hours, minutes, seconds)
+    secondsContainer.textContent = seconds < 10 ? "0" + seconds : seconds
+    minutesContainer.textContent = minutes < 10 ? "0" + minutes : minutes
+    hoursContainer.textContent = hours < 10 ? "0" + hours : hours
+    daysContainer.textContent = days < 10 ? "0" + days : days
+    // console.log({days, hours, minutes, seconds})
 };
 
-updateCountdown();
+setInterval(updateCountdown, 1000);
+
+// updateCountdown();
