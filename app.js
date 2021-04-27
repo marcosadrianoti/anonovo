46c9ab117459bab9d6c9ec977f75959ef3bda749
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-04-27 17:55:24 -0300

    anonovo done

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ const countdownContainer = document.querySelector('#countdown');
 
 const nextYear = new Date().getFullYear() + 1;
 const newYearTime = new Date(`january 01 ${nextYear} 00:00:00`);
+
 nextYearContainer.textContent = nextYear;
+
+const getTimeUnit = unit => unit < 10 ? "0" + unit : unit
+
+const insertCountdownValues = ({days, hours, minutes, seconds}) => {
+    secondsContainer.textContent = getTimeUnit(seconds);
+    minutesContainer.textContent = getTimeUnit(minutes);
+    hoursContainer.textContent = getTimeUnit(hours);
+    daysContainer.textContent = getTimeUnit(days);
+}
 
 const updateCountdown = () => {
     const currentTime = new Date();
@@ -18,18 +28,14 @@ const updateCountdown = () => {
     const minutes = Math.floor(difference / 1000 / 60) % 60;
     const seconds = Math.floor(difference / 1000) % 60;
 
-    secondsContainer.textContent = seconds < 10 ? "0" + seconds : seconds
-    minutesContainer.textContent = minutes < 10 ? "0" + minutes : minutes
-    hoursContainer.textContent = hours < 10 ? "0" + hours : hours
-    daysContainer.textContent = days < 10 ? "0" + days : days
-    // console.log({days, hours, minutes, seconds})
+    insertCountdownValues({days, hours, minutes, seconds})
 };
 
-setTimeout(() => {
+const handleCountdownDiplay = () => {
     spinnerLoading.remove();
     countdownContainer.style.display = "flex";
-}, 1000);
+}
+
+setTimeout(handleCountdownDiplay, 1000);
 
 setInterval(updateCountdown, 1000);
-
-// updateCountdown();
